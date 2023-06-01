2afe5f07f098c3c672e899e7dff4d5d427056458
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-06-02 00:44:40 +0200

    ad1 fix2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,7 @@ const body = document.body;
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 stopBtn.disabled = true;
-
+let timerId = null; 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
