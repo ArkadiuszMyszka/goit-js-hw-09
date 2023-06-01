cfdd320a7531868c5cbe6839276425dddf7ccd3f
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-06-01 21:12:45 +0200

    ad1. done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', () => {
+  changeBgColor();
+  timerId = setInterval(changeBgColor, 1000);
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+});
